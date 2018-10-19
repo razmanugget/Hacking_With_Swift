87f3441b6acb2cd16e95c04c30409b93ec1901c3
raz <rami_s@hotmail.com>
2018-10-19 07:52:20 +0200

    Chapter 12b

---
+++ 12b User Defaults - Codable/Names to Faces/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Names to Faces
+//
+//  Created by home on 5/14/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+  var name: String
+  var image: String
+  
+  init(name: String, image: String) {
+    self.name = name
+    self.image = image
+  }
+}
