f301c0116a97ba928a21bdd9f8033eae36d7cf62
Razmanugget <rami_s@hotmail.com>
2018-10-29 20:29:17 +0100

    updated to Swift 4.2 in Xcode 10

---
+++ 10 Names to Faces/Names to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by home on 12/4/17.
+//  Copyright © 2017 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var name: UILabel!
+}
