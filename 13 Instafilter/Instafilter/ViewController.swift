5d74abe963390d19eee0e592eb7f05f2965b1454
Razmanugget <rami_s@hotmail.com>
2018-10-31 18:49:56 +0100

    updated to Swift 4.2 in Xcode 10

--- 13 Instafilter/Instafilter/ViewController.swift
+++ 13 Instafilter/Instafilter/ViewController.swift
@@ -42,8 +42,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   }
   
   // MARK: - Functions
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-    guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {return}
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+    guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage else {return}
     dismiss(animated: true)
     currentImage = image
     
@@ -118,3 +121,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
 }
 
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
